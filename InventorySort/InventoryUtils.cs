f43e9596804ae8b12751d477a45fc79dcb46d932
end360 <end360@users.noreply.github.com>
2021-02-23 02:48:47 -0500

    EquipmentAndQuickSlots Support
    Modified the inventory sorting so that if you have the EquipmentAndQuickSlots mod installed it won't sort items out of those slots.

--- InventorySort/InventoryUtils.cs
+++ InventorySort/InventoryUtils.cs
@@ -3,18 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using UnityEngine;
+using BepInEx;
+using System.Reflection;
 namespace InventorySort
 {
     public static class InventoryUtils
     {
+        static MethodInfo IsEquipmentSlot;
+        static MethodInfo IsQuickSlot;
+
+        public static bool HasPlugin(string guid)
+        {
+            var plugins = UnityEngine.Object.FindObjectsOfType<BaseUnityPlugin>();
+
+            return plugins.Any(plugin => plugin.Info.Metadata.GUID == guid);
+        }
+
+        public static bool ShouldSortItem(Vector2i itemPos, Vector2i offset)
+        {
+            if (HasPlugin("randyknapp.mods.equipmentandquickslots"))
+            {
+                Plugin.instance.GetLogger().LogDebug("Found EquipmentAndQuickSlots plugin");
+                if (IsEquipmentSlot == null && IsQuickSlot == null)
+                {
+                    var ass = Assembly.Load("EquipmentAndQuickSlots");
+                    if (ass != null)
+                    {
+                        Plugin.instance.GetLogger().LogDebug("Found assembly");
+                        var type = ass.GetTypes().First(a => a.IsClass && a.Name == "EquipmentAndQuickSlots");
+                        var pubstatic = type.GetMethods(BindingFlags.Public | BindingFlags.Static);
+                        IsEquipmentSlot = pubstatic.First(t => t.Name == "IsEquipmentSlot" && t.GetParameters().Length == 1);
+                        Plugin.instance.GetLogger().LogDebug($"IsEquipmentSlot: {IsEquipmentSlot}");
+                        IsQuickSlot = pubstatic.First(t => t.Name == "IsQuickSlot" && t.GetParameters().Length == 1);
+                        Plugin.instance.GetLogger().LogDebug($"IsQuickSlot: {IsQuickSlot}");
+
+                    }
+                }
+
+                if ((bool) IsEquipmentSlot?.Invoke(null, new object[] { itemPos }))
+                    return false;
+                if ((bool)IsQuickSlot?.Invoke(null, new object[] { itemPos }))
+                    return false;
+            }
+
+            return itemPos.y > offset.y || (itemPos.y == offset.y && itemPos.x >= offset.x);
+        }
+
         public static void Sort(Inventory inventory, int offset = 0)
         {
             inventory.GetAllItems().Sort((a, b) => a.m_shared.m_name.CompareTo(b.m_shared.m_name));
             var offsetv = new Vector2i(offset % inventory.GetWidth(), offset / inventory.GetWidth());
 
             var i = offset;
-            foreach (var item in inventory.GetAllItems().Where(itm => itm.m_gridPos.y > offsetv.y || (itm.m_gridPos.y == offsetv.y && itm.m_gridPos.x >= offsetv.x)))
+            foreach (var item in inventory.GetAllItems().Where(itm => ShouldSortItem(itm.m_gridPos, offsetv)))
             {
                 var x = i % inventory.GetWidth();
                 var y = i / inventory.GetWidth();
